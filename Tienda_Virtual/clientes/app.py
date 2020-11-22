f9a1cec0c8f751ae3d01f36781d609fa007718ab
Julio Morales <julioscorpion.007@gmail.com>
2020-11-21 21:38:32 -0500

    Tienda virtual
    Commit proyecto

---
+++ Tienda_Virtual/clientes/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, flash, url_for, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DB_Tienda.sqlite3'
+app.config['SECRET_KEY'] = "random string"
+
+db = SQLAlchemy(app)
+
+class clientes(db.Model):
+   id = db.Column('cliente_id', db.Integer, primary_key = True)
+   name = db.Column(db.String(100))
+
+   def __init__(self, name):       
+       self.name = name
+       
+
+
+@app.route("/clientes")
+
+def show_all():
+    listaclientes = clientes.query.all() 
+    return listaclientes
+
+# def clientes():
+#     return 
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
